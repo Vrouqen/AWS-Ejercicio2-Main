70a5c44b7ac36af2848d681772b21198a3913f69
AllanC2002 <134990755+AllanC2002@users.noreply.github.com>
2025-05-14 19:16:39 -0500

    Consumo de api Chuck norris

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/chuck_norris")
+def chuck_norris():
+    return render_template("chuck_norris.html")
 
 if __name__=="__main__":
     app.run(debug=True, host="0.0.0.0", port=777)
