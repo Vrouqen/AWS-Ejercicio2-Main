62756d980e071a6f7b4fa3393a6c6e08cc0afb2b
Vrouqen <108952975+Vrouqen@users.noreply.github.com>
2025-05-19 21:57:42 -0500

    Se marca la base de los Saas

--- app.py
+++ app.py
@@ -6,9 +6,13 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/chuck_norris")
-def chuck_norris():
-    return render_template("chuck_norris.html")
+@app.route("/correo")
+def correo():
+    return render_template("correo.html")
+
+@app.route("/instagram_followers")
+def instagram_followers():
+    return render_template("instagram_followers.html")
 
 if __name__=="__main__":
     app.run(debug=True, host="0.0.0.0", port=777)
